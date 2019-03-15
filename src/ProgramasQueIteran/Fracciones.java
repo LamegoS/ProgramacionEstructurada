91a9fa063a123824a5c140f7fc411710e1844f2a
LamegoS <46984197+LamegoS@users.noreply.github.com>
2019-03-14 23:01:39 -0600

    140319

--- src/ProgramasQueIteran/Fracciones.java
+++ src/ProgramasQueIteran/Fracciones.java
@@ -2,8 +2,21 @@ package ProgramasQueIteran;
 
 public class Fracciones 
 {
+    static float resultado = 1;
+    static int i;
     public static void MenosMas()
     {
-        //cambio
+        for 
+        (i=2; i<= 100; i+= 2)
+        {
+            if (i%4 == 0)
+            {
+                resultado = resultado +1.00f/i;
+            }
+            else
+                resultado= resultado -1.00f/i;
+        }
+        System.out.println(resultado);
     }
 }
+
